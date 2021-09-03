54ef2c0d16d5e82ac13aab1c035d7ef7a3d48dae
Linksonder <linksonder@gmail.com>
2021-09-03 09:53:22 +0200

    Week 1 - demo

---
+++ week1/DemoA/DemoA.domain/FortuneWeekendTeller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoA.domain
+{
+    public class FortuneWeekendTeller
+    {
+        public IsHetWeekend WeekendChecker()
+        {
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                return IsHetWeekend.Bietje;
+            }
+
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return IsHetWeekend.JaHetIsWeekend;
+            }
+
+            return IsHetWeekend.NeeNogLangeNieEllende;
+        }
+    }
+
+    public enum IsHetWeekend
+    {
+        JaHetIsWeekend, NeeNogLangeNieEllende, Bietje
+    }
+}
---
+++ week1/DemoA/DemoA/Program.cs
@@ -0,0 +1,27 @@
+﻿using DemoA.domain;
+using System;
+
+
+namespace DemoA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FortuneWeekendTeller teller = new FortuneWeekendTeller();
+
+            IsHetWeekend resultaat = teller.WeekendChecker();
+
+            Console.WriteLine("Welkom bij de weekend checker!");
+
+            switch(resultaat)
+            {
+                case IsHetWeekend.Bietje: Console.WriteLine("It's friday again!!! saturday sunday what!"); break;
+                case IsHetWeekend.JaHetIsWeekend: Console.WriteLine("Ik ga zwemmen in Bacardi lemouuun"); break;
+                default: Console.WriteLine("Nog lange nie nog lange nie!");break;    
+            }
+
+
+        }
+    }
+}
